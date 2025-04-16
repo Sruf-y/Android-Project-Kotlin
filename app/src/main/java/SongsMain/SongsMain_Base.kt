3adde093e51d4c1a989c47afe1511fdaa9a80018
Sruf-y <andreibotofan@gmail.com>
2025-04-17 02:51:36 +0300

    Made bottomsheet clickable(to fix a UX bug)

--- app/src/main/java/SongsMain/SongsMain_Base.kt
+++ app/src/main/java/SongsMain/SongsMain_Base.kt
@@ -2,19 +2,15 @@ package SongsMain
 
 
 import Utilities.Utils.Companion.dP
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import android.widget.TextView
-import androidx.collection.intIntMapOf
-import androidx.compose.ui.unit.lerp
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.Lifecycle.Event
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.viewpager2.widget.ViewPager2
 import com.example.composepls.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -22,6 +18,7 @@ import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import de.greenrobot.event.EventBus
+import kotlin.math.max
 
 
 class SongsMain_Base : Fragment(R.layout.fragment_songs_main__base) {
@@ -64,12 +61,41 @@ class SongsMain_Base : Fragment(R.layout.fragment_songs_main__base) {
             this.state= BottomSheetBehavior.STATE_COLLAPSED
         }
 
+
+
+
+
         // auto-rolling textview
         requireView().findViewById<TextView>(R.id.serviceColorCode).isSelected = true;
+
+        val main = requireView().findViewById<ConstraintLayout>(R.id.main)
 
 
         val scene1: ConstraintLayout = requireView().findViewById(R.id.scene1)
         val scene2: ConstraintLayout = requireView().findViewById(R.id.scene2)
+
+
+
+        val topSystemBar = activity?.windowManager?.currentWindowMetrics?.windowInsets?.getInsets(WindowInsetsCompat.Type.displayCutout())
+
+        Log.i("TESTS",topSystemBar?.left.toString()+"   "+topSystemBar?.right.toString()+"   "+topSystemBar?.top.toString()+"   "+topSystemBar?.bottom.toString()+" :topSystembars")
+
+        val bottomnavBar = activity?.windowManager?.currentWindowMetrics?.windowInsets?.getInsets(WindowInsetsCompat.Type.navigationBars())
+
+        Log.i("TESTS",bottomnavBar?.left.toString()+"   "+bottomnavBar?.right.toString()+"   "+bottomnavBar?.top.toString()+"   "+bottomnavBar?.bottom.toString())
+
+
+        if(resources.configuration.orientation== Configuration.ORIENTATION_LANDSCAPE) {
+            main.fitsSystemWindows=true
+
+            // NOT WORKING SADLY
+            if(bottomnavBar!=null)
+                bottomSheet.updatePadding(max(bottomnavBar.left,bottomnavBar.right),0,max(bottomnavBar.left,bottomnavBar.right),0)
+        }
+        else{
+            main.fitsSystemWindows=true
+
+        }
 
         behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
             override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -77,7 +103,7 @@ class SongsMain_Base : Fragment(R.layout.fragment_songs_main__base) {
                     BottomSheetBehavior.STATE_COLLAPSED->{
 
 
-                        scene2.visibility=View.GONE
+                        scene2.visibility=View.INVISIBLE
 
                         scene1.visibility=View.VISIBLE
                         scene1.alpha=1F
@@ -85,7 +111,7 @@ class SongsMain_Base : Fragment(R.layout.fragment_songs_main__base) {
                     BottomSheetBehavior.STATE_EXPANDED->{
 
 
-                        scene1.visibility=View.GONE
+                        scene1.visibility=View.INVISIBLE
 
                         scene2.visibility=View.VISIBLE
                         scene2.alpha=1F
@@ -110,9 +136,26 @@ class SongsMain_Base : Fragment(R.layout.fragment_songs_main__base) {
         })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
 
 
 
